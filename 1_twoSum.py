7074fac6e18087855efed2f0570b7f89d332c227
Hao Liu <hliu202@163.com>
2020-05-07 10:14:37 +0800

    1

---
+++ 1_twoSum.py
@@ -0,0 +1,14 @@
+nums = [2,7,11,15]
+target = 9
+
+hm = {}
+for i in range(len(nums)):
+    v = nums[i]
+    if target - v in hm:
+        print ([hm.get(target - v), i])
+        break
+    hm[v] = i
+
+# 29/29 cases passed (56 ms)
+# Your runtime beats 67.93 % of python3 submissions
+# Your memory usage beats 5.48 % of python3 submissions (15.1 MB)
