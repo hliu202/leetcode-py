7087b955344e34e69b41ae5dd35e420220d3313f
Hao Liu <hliu202@163.com>
2020-06-05 20:03:59 +0800

    daily

---
+++ interview/24_printMatrix.py
@@ -0,0 +1,11 @@
+# https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+
+class Solution:
+    def spiralOrder(self, matrix):
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            matrix = list(zip(*matrix))[::-1]
+        return res
+
+print (Solution().spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
