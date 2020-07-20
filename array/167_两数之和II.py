4d00b0f7f2261f8e56ed47a50ddd862ddc96f38d
Hao Liu <hliu202@163.com>
2020-07-20 19:49:06 +0800

    daily

---
+++ array/167_两数之和II.py
@@ -0,0 +1,16 @@
+from util.common_imports import *
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] < target:
+                i += 1
+            else:
+                j -= 1
+
+print(Solution().twoSum(numbers=[2, 7, 11, 15], target=9))
+
