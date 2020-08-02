c682b1dabbf52d7e562a148256a1143f49107e1f
Hao Liu <hliu202@163.com>
2020-08-02 12:14:33 +0800

    daily

---
+++ tree/114_二叉树展开.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from util.common_imports import *
+
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        preList = []
+
+        def preOrder(root):
+            if not root:
+                return
+
+            preList.append(root)
+            preOrder(root.left)
+            preOrder(root.right)
+
+        preOrder(root)
+        size = len(preList)
+        for i in range(1, size):
+            prev, curr = preList[i - 1], preList[i]
+            prev.left = None
+            prev.right = curr
+
