8691965fe8d6467d9507c672b29e6829c5b50869
Hao Liu <hliu202@163.com>
2020-09-21 19:51:07 +0800

    daily

---
+++ tree/538_二叉搜索树转换为累加树_num.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from util.common_imports import *
+
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        def dfs(root: TreeNode):
+            nonlocal total
+            if root:
+                dfs(root.right)
+                total += root.val
+                root.val = total
+                dfs(root.left)
+
+        # 用一个变量，减少参数传递
+        total = 0
+        dfs(root)
+        return root
+
+
+printTree(Solution().convertBST(createTree("[5,2,13]")))
